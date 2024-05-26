c8aef60162da9b19aedda8174b1efda81e863487
bundana <>
2024-05-26 22:26:24 +0000

    Update text in App component

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>My name is Sulemana Abdul-Haafiz Bundana</Text>
       <StatusBar style="auto" />
     </View>
   );
